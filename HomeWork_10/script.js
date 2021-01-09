f84f89e6cff8f55500a3bf8bf069076f76c72482
Oksana Iastrebtsova <oksana.iastrebtsova@gmail.com>
2021-01-09 20:25:42 +0200

    Занятие 10. Книга контактов

---
+++ HomeWork_10/script.js
@@ -0,0 +1,63 @@
+const styledTableBody = document.querySelector('#styledTableBody');
+const firstNameField = document.querySelector('#firstNameField');
+const lastNameField = document.querySelector('#lastNameField');
+const phoneField = document.querySelector('#phoneField');
+
+document.querySelector('#styledTable')
+    .addEventListener('click', deleteRow);
+
+document.querySelector('#addRowButton')
+    .addEventListener('click', onButtonAddRowClick);
+
+function deleteRow(event) {
+    event.target.parentElement.remove();
+}
+
+function onButtonAddRowClick() {
+    if (firstNameField.value.trim() && lastNameField.value.trim() && phoneField.value.trim()) {
+        addNewRow(firstNameField.value.trim(), lastNameField.value.trim(), phoneField.value.trim());
+        clearFields();
+    }
+}
+
+function addNewRow(firstName, lastName, phone) {
+    styledTableBody.appendChild(createNewRow(firstName, lastName, phone) );
+}
+
+function createNewRow(firstName, lastName, phone) {
+    const row = document.createElement('tr');
+    row.appendChild(createCell(firstName));
+    row.appendChild(createCell(lastName));
+    row.appendChild(createCell(phone));
+    row.appendChild(createButton('Удалить'));
+    return row;
+}
+
+function createCell(text) {
+    const cell = document.createElement('td');
+    const cellText = document.createTextNode(text);
+    cell.appendChild(cellText);
+    return cell;
+}
+
+function createButton(buttonText) {
+    const button = document.createElement('input');
+    button.type = 'button';
+    button.value = buttonText;
+    return button;
+}
+
+function clearFields() {
+    firstNameField.value = '';
+    lastNameField.value = '';
+    phoneField.value = '';
+}
+
+
+
+
+
+
+
+
+
