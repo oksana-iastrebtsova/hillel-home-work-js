2a2df76b1187cdeebe17a6386c7854f6f906b78c
Oksana Iastrebtsova <oksana.iastrebtsova@gmail.com>
2021-01-23 23:40:09 +0200

    Занятие 13. Решение после лекции 14

--- HomeWork_13/script.js
+++ HomeWork_13/script.js
@@ -1,51 +1,66 @@
+class Component {
 
-function Component(price, calories) {
-    this._price = price;
-    this._calories = calories;
+    constructor(price, calories) {
+        this._price = price;
+        this._calories = calories;
+    }
+
+    getPrice() {
+        return this._price;
+    }
+
+    getCalories() {
+        return this._calories;
+    }
 }
 
-Component.prototype.getPrice = function() {
-    return this._price;
+class HamburgerBase extends Component {
+    constructor(price, calories) {
+        super(price, calories);
+    }
 }
 
-Component.prototype.getCalories = function() {
-    return this._calories;
+class Topping extends Component {
+    constructor(price, calories) {
+        super(price, calories);
+    }
 }
 
-function Hamburger(component) {
-    this._price = component.getPrice();
-    this._calories = component.getCalories();
+class Hamburger extends Component {
+
+    static BASE_SMALL = new HamburgerBase(50, 20);
+    static BASE_MEDIUM = new HamburgerBase(75, 30);
+    static BASE_LARGE = new HamburgerBase(100, 40);
+
+    static TOPPING_CHEESE = new Topping(10, 20);
+    static TOPPING_POTATO = new Topping(15, 10);
+    static TOPPING_SALAD = new Topping(20, 5);
+    static TOPPING_SEASON = new Topping(15, 0);
+    static TOPPING_MAYO = new Topping(20, 5);
+
+    constructor(name, hamburgerBase) {
+        super(hamburgerBase.getPrice(), hamburgerBase.getCalories());
+        this._name = name;
+    }
+
+    addTopping(topping) {
+        this._price += topping.getPrice();
+        this._calories += topping.getCalories();
+    }
+
+    getName() {
+        return this._name;
+    }
+
+    toString() {
+        return 'Hamburger {Name: ' + this.getName() + ', Price: ' + this.getPrice() + ', Calories: ' + this.getCalories() + '}';
+    }
 }
 
-Hamburger.prototype = new Component();
-
-Hamburger.prototype.addTopping = function(topping) {
-    this._price += topping.getPrice();
-    this._calories += topping.getCalories();
-}
-
-function Topping(component) {
-    this._price = component.getPrice();
-    this._calories = component.getCalories();
-}
-
-Topping.prototype = new Component();
-
-Hamburger.SIZE_SMALL = new Component(50, 20);
-Hamburger.SIZE_MEDIUM = new Component(75, 30);
-Hamburger.SIZE_LARGE = new Component(100, 40);
-Hamburger.TOPPING_CHEESE = new Topping(new Component(10, 20));
-Hamburger.TOPPING_POTATO = new Topping(new Component(15, 10));
-Hamburger.TOPPING_SALAD = new Topping(new Component(20, 5));
-Hamburger.TOPPING_SEASON = new Topping(new Component(15, 0));
-Hamburger.TOPPING_MAYO = new Topping(new Component(20, 5));
-
-
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL);
-hamburger.addTopping(Topping.TOPPING_POTATO);
-
-console.log("Price: " + hamburger.getPrice());
-console.log("Calories: " + hamburger.getCalories());
+const hamburger = new Hamburger("MyFirstBurger", Hamburger.BASE_SMALL);
+hamburger.addTopping(Hamburger.TOPPING_POTATO);
+hamburger.addTopping(Hamburger.TOPPING_CHEESE);
+console.log(hamburger.toString());
 
 
 
